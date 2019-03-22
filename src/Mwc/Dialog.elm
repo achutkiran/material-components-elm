eb2cf16045890004107e6130d0be1f543924fde0
Achut Cherukuri <Achut.Cherukuri@primeroedge.com>
2019-03-21 20:37:53 -0500

    dialog update

--- src/Mwc/Dialog.elm
+++ src/Mwc/Dialog.elm
@@ -9,7 +9,7 @@ Used Elm-css for Styling
 
 import Css exposing (..)
 import Css.Global
-import Html.Styled exposing (Attribute, Html, div, h2, node, text)
+import Html.Styled exposing (Attribute, Html, div, h2, node, span, text)
 import Html.Styled.Attributes as Attr
 import Json.Encode as Encode
 
@@ -127,48 +127,71 @@ view properties =
         config =
             fetchConfig properties
     in
-    node "paper-dialog"
-        (fetchProperties config)
-        [ h2
-            [ Attr.css
-                [ textAlign start
-                , color (rgba 0 0 0 0.87)
-                , paddingLeft (px 24)
-                , fontFamilies [ "Roboto" ]
-                , fontSize (px 20)
-                , color (rgba 0 0 0 0.87)
-                , marginTop (px 24)
-                ]
-            ]
-            [ text config.title ]
-        , div
-            [ Attr.css
-                [ paddingBottom (px 28)
-                , color (rgba 0 0 0 0.6)
-                , textAlign start
-                , if config.fullScreen then
-                    maxHeight (pct 100)
-
-                  else
-                    maxHeight (px 570)
-                ]
-            ]
-            config.body
-        , div
-            [ Attr.css
-                [ displayFlex
-                , flexWrap wrap
-                , justifyContent flexEnd
-                , margin (px 0)
-                , paddingBottom (px 8)
-                , paddingRight (px 8)
-                , Css.Global.children
-                    [ Css.Global.typeSelector "*"
-                        [ paddingLeft (px 8) ]
+    span
+        []
+        [ node "paper-dialog"
+            (fetchProperties config)
+            [ h2
+                [ Attr.css
+                    [ textAlign start
+                    , color (rgba 0 0 0 0.87)
+                    , paddingLeft (px 24)
+                    , fontFamilies [ "Roboto" ]
+                    , fontSize (px 20)
+                    , color (rgba 0 0 0 0.87)
+                    , marginTop (px 24)
                     ]
                 ]
+                [ text config.title ]
+            , div
+                [ Attr.css
+                    [ paddingBottom (px 28)
+                    , color (rgba 0 0 0 0.6)
+                    , textAlign start
+                    , overflow auto
+                    , if config.fullScreen then
+                        Css.batch
+                            [ maxHeight (calc (vh 100) minus (px 116))
+                            , boxSizing borderBox
+                            ]
+
+                      else
+                        maxHeight (px 570)
+                    ]
+                ]
+                config.body
+            , div
+                [ Attr.css
+                    [ displayFlex
+                    , flexWrap wrap
+                    , justifyContent flexEnd
+                    , margin (px 0)
+                    , paddingBottom (px 8)
+                    , paddingRight (px 8)
+                    , Css.Global.children
+                        [ Css.Global.typeSelector "*"
+                            [ paddingLeft (px 8) ]
+                        ]
+                    ]
+                ]
+                config.actionBar
             ]
-            config.actionBar
+        , if config.opened == Visible then
+            div
+                [ Attr.css
+                    [ position fixed
+                    , top (px 0)
+                    , left (px 0)
+                    , width (vw 100)
+                    , height (vh 100)
+                    , backgroundColor (rgba 0 0 0 0.6)
+                    , zIndex (int 102)
+                    ]
+                ]
+                []
+
+          else
+            text ""
         ]
 
 
@@ -207,14 +230,14 @@ fetchProperties config =
 
         Hidden ->
             Attr.property "opened" (Encode.bool False)
-    , Attr.property "modal" (Encode.bool True)
+    , Attr.attribute "no-cancel-on-outside-click" ""
+    , Attr.attribute "no-cancel-on-esc-key" ""
     , Attr.css
         [ borderRadius (px 4)
         , if config.fullScreen then
             Css.batch
                 [ maxWidth (pct 100)
                 , maxHeight (pct 100)
-                , overflowY scroll
                 ]
 
           else
